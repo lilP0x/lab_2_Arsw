0b2c76b4dcc19f7b47ea1cfafc5ca288e4c70871
lalaro <100946295+lalaro@users.noreply.github.com>
2024-08-26 20:42:57 -0500

    TallerPunto1/a y b

--- parte2/src/main/java/arsw/threads/MainCanodromo.java
+++ parte2/src/main/java/arsw/threads/MainCanodromo.java
@@ -1,16 +1,11 @@
 package arsw.threads;
-
+import javax.swing.JButton;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-
-import javax.swing.JButton;
-
 public class MainCanodromo {
 
     private static Galgo[] galgos;
-
     private static Canodromo can;
-
     private static RegistroLlegada reg = new RegistroLlegada();
 
     public static void main(String[] args) {
@@ -18,54 +13,55 @@ public class MainCanodromo {
         galgos = new Galgo[can.getNumCarriles()];
         can.setVisible(true);
 
-        //Acción del botón start
-        can.setStartAction(
-                new ActionListener() {
+        // Acción del botón Start
+        can.setStartAction(new ActionListener() {
+            @Override
+            public void actionPerformed(final ActionEvent e) {
+                //como acción, se crea un nuevo hilo que cree los hilos
+                //'galgos', los pone a correr, y luego muestra los resultados.
+                //La acción del botón se realiza en un hilo aparte para evitar
+                //bloquear la interfaz gráfica.
+                ((JButton) e.getSource()).setEnabled(false);
+                new Thread() {
+                    public void run() {
+                        for (int i = 0; i < can.getNumCarriles(); i++) {
+                            //crea los hilos 'galgos'
+                            galgos[i] = new Galgo(can.getCarril(i), "" + i, reg);
+                            //inicia los hilos
+                            galgos[i].start();
+                        }
 
-                    @Override
-                    public void actionPerformed(final ActionEvent e) {
-						//como acción, se crea un nuevo hilo que cree los hilos
-                        //'galgos', los pone a correr, y luego muestra los resultados.
-                        //La acción del botón se realiza en un hilo aparte para evitar
-                        //bloquear la interfaz gráfica.
-                        ((JButton) e.getSource()).setEnabled(false);
-                        new Thread() {
-                            public void run() {
-                                for (int i = 0; i < can.getNumCarriles(); i++) {
-                                    //crea los hilos 'galgos'
-                                    galgos[i] = new Galgo(can.getCarril(i), "" + i, reg);
-                                    //inicia los hilos
-                                    galgos[i].start();
-
-                                }
-                               
-				can.winnerDialog(reg.getGanador(),reg.getUltimaPosicionAlcanzada() - 1); 
-                                System.out.println("El ganador fue:" + reg.getGanador());
+                        try {
+                            // Esperar a que todos los galgos terminen
+                            for (Galgo galgo : galgos) {
+                                galgo.join();
                             }
-                        }.start();
+                        } catch (InterruptedException ex) {
+                            ex.printStackTrace();
+                        }
+                        
+                        can.winnerDialog(reg.getGanador(), reg.getUltimaPosicionAlcanzada() - 1);
+                        System.out.println("El ganador fue: " + reg.getGanador());
 
+                        // Volver a habilitar el botón Start
+                        ((JButton) e.getSource()).setEnabled(true);
                     }
-                }
-        );
+                }.start();
+            }
+        });
 
-        can.setStopAction(
-                new ActionListener() {
-                    @Override
-                    public void actionPerformed(ActionEvent e) {
-                        System.out.println("Carrera pausada!");
-                    }
-                }
-        );
+        can.setStopAction(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.out.println("Carrera pausada!");
+            }
+        });
 
-        can.setContinueAction(
-                new ActionListener() {
-                    @Override
-                    public void actionPerformed(ActionEvent e) {
-                        System.out.println("Carrera reanudada!");
-                    }
-                }
-        );
-
+        can.setContinueAction(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.out.println("Carrera reanudada!");
+            }
+        });
     }
-
 }
